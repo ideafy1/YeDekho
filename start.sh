43da283394371e71d8c2e975880384d31d245d0d
Aditya Singh <118215356+ideafy1@users.noreply.github.com>
2023-07-27 13:50:33 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,10 +2,10 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/mrmalikoffl/MsFilmFactoryV2
+  git clone https://github.com/ideafy1/yedekho
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /MsFilmFactoryV2
+  git clone $UPSTREAM_REPO /yedekho
 fi
 cd /LazyPrincessV2
 pip3 install -U -r requirements.txt
